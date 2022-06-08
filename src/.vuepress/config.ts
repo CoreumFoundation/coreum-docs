ad0ae408d0593d398f280bedfe886ecf25ba8771
karamvirsingh98 <67458484+karamvirsingh98@users.noreply.github.com>
2022-06-08 15:01:41 -0700

    refactor navbar

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,81 +1,5 @@
 import { defaultTheme, defineUserConfig } from "vuepress";
-import { NavbarConfig } from "vuepress";
-
-const navbar: NavbarConfig = [
-  {
-    text: "Home",
-    link: "/",
-  },
-  {
-    text: "Guide",
-    link: "/guide",
-  },
-  {
-    text: "Ecosystem",
-    children: [
-      {
-        text: "Tokens",
-        children: [
-          {
-            text: "Overview",
-            link: "/tokens",
-          },
-          {
-            text: "Issuance",
-            link: "/tokens/issuance",
-          },
-        ],
-      },
-      {
-        text: "Smart Contracts",
-        children: [
-          {
-            text: "Overview",
-            link: "/contracts"
-          }
-        ]
-      }
-    ],
-  },
-  {
-    text: "Components",
-    children: [
-      {
-        text: "Coreznet",
-        link: "/coreznet",
-      },
-      {
-        text: "Explorer",
-        link: "/explorer",
-      },
-      {
-        text: "Monitoring",
-        link: "/monitoring",
-      },
-      {
-        text: "Tools",
-        link: "/tools",
-      },
-    ],
-  },
-  {
-    text: "Links",
-    children: [
-      {
-        text: "Main Page",
-        link: "https://coreum.com",
-      },
-      {
-        text: "Coreum Block Explorer",
-        link: "https://coreum.com",
-      },
-      {
-        text: "Github",
-        link: "https://github.com/CoreumFoundation",
-      },
-    ],
-  },
-];
+import { navbar } from "./configs/navbar";
 
 export default defineUserConfig({
   lang: "en-us",
@@ -83,10 +7,6 @@ export default defineUserConfig({
   description:
     "Documentation for Coreum, a Third-Generation Layer 1 Blockchain",
   theme: defaultTheme({
-    locales: {
-      "/": {
-        navbar,
-      },
-    },
+    navbar,
   }),
 });
---
+++ src/.vuepress/configs/navbar.ts
@@ -0,0 +1,77 @@
+import { NavbarConfig } from "vuepress";
+
+export const navbar: NavbarConfig = [
+  {
+    text: "Home",
+    link: "/",
+  },
+  {
+    text: "Guide",
+    link: "/guide/",
+  },
+  {
+    text: "Ecosystem",
+    children: [
+      {
+        text: "Tokens",
+        children: [
+          {
+            text: "Overview",
+            link: "/tokens/",
+          },
+          {
+            text: "Token Issuance",
+            link: "/tokens/issuance",
+          },
+        ],
+      },
+      {
+        text: "Smart Contracts",
+        children: [
+          {
+            text: "Overview",
+            link: "/contracts",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "Components",
+    children: [
+      {
+        text: "Coreznet",
+        link: "/coreznet",
+      },
+      {
+        text: "Explorer",
+        link: "/explorer",
+      },
+      {
+        text: "Monitoring",
+        link: "/monitoring",
+      },
+      {
+        text: "Tools",
+        link: "/tools",
+      },
+    ],
+  },
+  {
+    text: "Links",
+    children: [
+      {
+        text: "Main Page",
+        link: "https://coreum.com",
+      },
+      {
+        text: "Coreum Block Explorer",
+        link: "https://coreum.com",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/CoreumFoundation",
+      },
+    ],
+  },
+];
